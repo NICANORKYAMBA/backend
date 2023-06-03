c9a1add88c9d3bd2bca3f8c2294ec0ff14a40c8c
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-06-03 16:36:52 +0300

    Edited User model

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
-    googleId: { type: String, default: null },
     timezone: { type: String, default: 'Africa/Nairobi' },
 });
 
